b757261029163060db2f7d224aaebe21982d6458
Austin Espinoza <d00416410@CMP-SCC112-1111.local>
2024-07-11 10:31:14 -0600

    added a create for days and a way to add and delete them

--- public/app.js
+++ public/app.js
@@ -12,6 +12,16 @@ Vue.createApp({
         email: "",
         password: "",
       },
+      searchInput: "",
+      newDay: {
+        title: "",
+        workouts: [],
+      },
+      newWorkout: [
+        {
+          work: "",
+        },
+      ],
     };
   },
   methods: {
@@ -21,6 +31,7 @@ Vue.createApp({
       let data = await response.json();
       this.workouts = data;
       console.log(data);
+      console.log("hello");
     },
 
     getDays: async function () {
@@ -29,6 +40,7 @@ Vue.createApp({
       let data = await response.json();
       this.days = data;
       console.log(data);
+      console.log("hello");
     },
 
     getWeeks: async function () {
@@ -37,11 +49,68 @@ Vue.createApp({
       let data = await response.json();
       this.weeks = data;
       console.log(data);
+      console.log("hello");
     },
     setPage: function (page) {
       this.currentPage = page;
     },
+    addworkout: function () {
+      this.newWorkout.push({
+        work: {},
+      });
+    },
+    removeWorkout: function (index) {
+      this.newWorkout.splice(index);
+      console.log(this.newWorkout);
+    },
+    createDay: async function () {
+      let myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+      this.newDay.workouts = this.newWorkout;
+      let requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: JSON.stringify(this.newDay),
+      };
+      let response = await fetch(`${URL}/days`, requestOptions);
+      if (response.status === 201) {
+        this.getDays();
+        this.clearday();
+        this.currentPage = "Browse";
+        console.log("Succesfully created");
+      } else {
+        console.log("Failed");
+      }
+    },
+    clearday: function () {
+      this.newDay = {
+        title: "",
+        workouts: [],
+      };
+    },
   },
+  computed: {
+    filteredDays: function () {
+      return this.days.filter((day) => {
+        return day.name.toLowerCase().includes(this.searchInput.toLowerCase());
+        console.log(this.days);
+      });
+    },
+
+    filteredWeeks: function () {
+      return this.weeks.filter((week) => {
+        return week.name.toLowerCase().includes(this.searchInput.toLowerCase());
+      });
+    },
+    filteredWorkouts: function () {
+      return this.workouts.filter((workout) => {
+        return workout.name
+          .toLowerCase()
+          .includes(this.searchInput.toLowerCase());
+      });
+    },
+  },
+
   created: function () {
     console.log("app loaded");
     this.getWorkouts();
